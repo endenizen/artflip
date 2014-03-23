f4915460626827da83a2185952189708dcba46d2
Brian Ferrell <brian.ferrell@rd.io>
2014-03-22 20:01:36 -0700

    Switch to rdio js api

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ It's like the iTunes album art screensaver, but for Rdio.
 Try it out
 ----------
 
-artflip is running on heroku at [http://artflip.herokuapp.com](http://artflip.herokuapp.com)
+artflip is running on github at [http://endenizen.net/artflip](http://endenizen.net/artflip)
 
 Screensaver
 -----------
@@ -18,52 +18,18 @@ Install the screensaver here: [https://dl.dropbox.com/u/120686/ArtFlipSaver.save
 How does it work?
 -----------------
 
-The backend is a flask app that communicates with the rdio api. The front end is javascript that requests the data, arranges the images and juggles css classes which perform the actual flipping using webkit css transitions. The 'options' from the screensaver are passed in the url like so:
+The app is written almost entirely in javascript which requests the data, arranges the images and juggles css classes which perform the actual flipping using webkit css transitions. The 'options' from the screensaver are passed in the url like so:
 
-    http://artflip.herokuapp.com/#user=endenizen;type=top;rows=3;delay=3
+    http://endenizen.net/artflip/#user=endenizen;type=top;rows=3;delay=3
 
 Installation
 ------------
 
-Create a virtualenv*
+artflip has no requirements. You can serve it on any webserver that serves static files, like github! When developing, I use something like the following:
 
-    virtualenv ENV
+    python -m SimpleHTTPServer 5000
 
-Activate it
-
-    source ENV/bin/activate
-
-Install dependencies
-
-    pip install -r requirements.txt
-
-Generate a secret (you can use 'bla' for local purposes). [More info](http://flask.pocoo.org/docs/quickstart/) on flask setup.
-
-    >>> import os
-    >>> os.urandom(24)
-    '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
-
-Get an Rdio api key and secret from the [Rdio developer site](http://developer.rdio.com/)
-
-Create env.sh
-
-    export APP_SECRET=your_secret
-    export RDIO_API_KEY=your_rdio_api_key
-    export RDIO_API_SECRET=your_rdio_api_secret
-
-Activate env vars
-
-    source env.sh
-
-Run app
-
-    python web.py
-
-That's it! Now (if all went accordingly) you're running the app locally and you can access it through:
+Which will make the app available on:
 
     http://localhost:5000
 
-A note on virtual env's
------------------------
-
-If you don't yet have a virtualenv routine, why not start now! A virtualenv keeps packages for a certain app separate from your system packages. They're installed into the directory you specify (I used 'ENV' but you can name it anything) and they're activated with the source command. You can even create a virtualenv for a different version of python, in case you are using packages that don't support the latest version. Finally, remember that your virtualenv is your own, and shouldn't be checked into git. I personally use a global gitignore file to make this easy. For more info, and more customization, check out the [documentation](http://www.virtualenv.org/en/latest/index.html).
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Depends on rdio.js
+
 (function() {
 
 var App = function(options) {
@@ -7,7 +9,8 @@ var App = function(options) {
     rows: 3,
     delay: 3,
     type: 'top',
-    username: null
+    user: null,
+    debug: false
   });
 
   // Never load more than this many albums
@@ -17,10 +20,15 @@ var App = function(options) {
   // Album lookup of key -> this.albums index
   this.albumLookup = {};
   this.init();
+
+  if (this.options.debug) {
+    console.log('setting up debug print interval');
+    this._debugPrintInterval = window.setInterval(_.bind(this.debugPrint, this), 500);
+  }
 };
 
 App.prototype.init = function() {
-  _.bindAll(this);
+  _.bindAll(this, 'resize', 'startFlip');
 
   // Setup initial size and bind resize
   $(window).on('resize', this.resize);
@@ -28,7 +36,21 @@ App.prototype.init = function() {
 
   this.content = $('#content').find('.container');
 
-  this.getAlbums(this.startFlip);
+  var that = this;
+
+  if (this.options.user && this.options.type != 'top') {
+    Rdio.get('findUser', {
+      vanityName: that.options.user,
+      extras: '-*,key'
+    }, function(response) {
+      if (response && response.result) {
+        that.options.userKey = response.result.key;
+      }
+      that.getAlbums(that.startFlip);
+    });
+  } else {
+    this.getAlbums(this.startFlip);
+  }
 };
 
 // Add album to albums array if it doesn't exist already
@@ -41,35 +63,90 @@ App.prototype.addAlbum = function(album) {
   }
 };
 
-// Get list of albums from `get_albums` api. Keep scraping
-// while incrementing `offset` until we get a response with
-// no new albums. Call `k` when done loading albums.
+// Load albums depending on App.options.type in chunks until
+// we run out or hit the limit.
+// Call `k` when done.
 App.prototype.getAlbums = function(k) {
   var that = this;
   // Scan collection
   function getChunk() {
-    $.ajax({
-      url: '/get_albums',
-      data: {
-        type: that.options.type,
-        user: that.options.user,
-        offset: that.albums.length
-      },
-      success: function(result) {
-        var albums = result.albums;
-        var added = 0;
+    var content = {};
+    var method = '';
+    var postProcess;
 
-        _.each(albums, function(album) {
-          added += !!that.addAlbum(album) ? 1 : 0;
-        });
+    switch(that.options.type) {
+      case 'collection':
+        method = 'getAlbumsInCollection';
+        content = {
+          user: that.options.userKey,
+          start: that.albums.length,
+          extras: '-*,albumKey,icon',
+          count: 50
+        };
+        postProcess = function(response) {
+          if (!response) {
+            return;
+          }
+          var result = response.result;
+          var newResult = [];
+          _.each(result, function(item) {
+            item.key = item.albumKey;
+            delete item.albumKey;
+            newResult.push(item);
+          });
+          response.result = newResult;
+          return response;
+        };
+        break;
+      case 'heavyrotation':
+        method = 'getHeavyRotation';
+        content = {
+          user: that.options.userKey,
+          start: that.albums.length,
+          count: 50,
+          type: 'albums',
+          extras: '-*,key,icon'
+        };
+        break;
+      case 'friendsheavyrotation':
+        method = 'getHeavyRotation';
+        content = {
+          user: that.options.userKey,
+          start: that.albums.length,
+          count: 50,
+          type: 'albums',
+          friends: 'true'
+        };
+        break;
+      default:
+        method = 'getTopCharts';
+        content = {
+          type: 'Album',
+          extras: '-*,key,icon',
+          start: that.albums.length,
+          count: 50
+        };
+        break;
+    }
 
-        // If we added at least one album, we got new data, otherwise, we might be looping! So just get started.
-        // Also, don't get more than `ALBUM_LIMIT` albums.
-        if (added > 0 && that.albums.length < that.ALBUM_LIMIT) {
-          getChunk();
-        } else {
-          k();
-        }
+    Rdio.get(method, content, function(response) {
+      if (postProcess) {
+        response = postProcess(response);
+      }
+
+      var albums = response.result;
+      var added = 0;
+
+      _.each(albums, function(album) {
+        added += !!that.addAlbum(album) ? 1 : 0;
+      });
+
+      // If we added at least one album, we got new data, otherwise, we might be looping! So just get started.
+      // Also, don't get more than `ALBUM_LIMIT` albums.
+      if (added > 0 && that.albums.length < that.ALBUM_LIMIT) {
+        getChunk();
+      } else {
+        k();
       }
     });
   }
@@ -184,6 +261,36 @@ App.prototype.getAlbumArt = function(index) {
   return icon;
 };
 
+App.prototype.debugPrint = function() {
+  if (!this._debugDiv) {
+    console.log('div didnt exist, creating');
+    this._debugDiv = $('<div id="debug"></div>').appendTo($('#content'));
+  }
+
+  var htmlDiv = $('<div></div>');
+
+  var data = {
+    albums: this.albums.length,
+    user: this.options.user,
+    type: this.options.type,
+    rows: this.options.rows,
+    cols: this.cols,
+    delay: this.options.delay,
+    'Window Width': this.prevWidth,
+    'Window Height': this.prevHeight,
+    size: this.size
+  };
+
+  var keys = _.keys(data);
+
+  _.each(keys, function(key) {
+    var row = $('<div></div>').text(key + ': ' + data[key]);
+    htmlDiv.append(row);
+  });
+
+  this._debugDiv.html(htmlDiv.html());
+};
+
 App.prototype.flipTime = function() {
   var col = this.randInt(this.cols);
   var row = this.randInt(this.options.rows);
@@ -251,6 +358,21 @@ function setupApp() {
   }
   var options = {};
   var parts;
+
+  // Convert options from a url like:
+  //
+  //   http://endenizen.net/artflip/#user=endenizen;type=collection;rows=3;delay=3;version=1";
+  //
+  // into:
+  //
+  //   {
+  //     user: 'endenizen',
+  //     type: 'collection',
+  //     rows: '3',
+  //     delay: '3',
+  //     version: '1'
+  //   }
+  //   
   try {
     parts = window.location.hash.substring(1).split(';');
     _.each(parts, function(part) {
@@ -260,6 +382,11 @@ function setupApp() {
   } catch (e) {
     options = {};
   }
+
+  if (window.location.host === 'localhost:5000') {
+    options.debug = true;
+  }
+
   window.app = new App(options);
 }
 
